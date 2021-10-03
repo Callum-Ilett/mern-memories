7207f271a7740df4782c3e8e87a3394b9bbc913c
Callum Ilett <cilett99@gmail.com>
2021-10-03 15:47:12 +0100

    Add env config and initial routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,5 @@
-import express from "express";
-
-const app = express();
-
-const PORT = 8000;
-
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+import app from "./app";
+import { PORT } from "./config";
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
