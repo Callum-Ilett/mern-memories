e4a55cd8b575889a145f0d9860fc72b988271623
Callum Ilett <cilett99@gmail.com>
2021-10-03 16:37:08 +0100

    Added connection to MongoDB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,15 @@
+import { connect as connectMongoDB } from "mongoose";
+import { MONGO_URI, PORT } from "./config";
 import app from "./app";
-import { PORT } from "./config";
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log("Connecting to MongoDB...");
+
+  try {
+    await connectMongoDB(MONGO_URI);
+    console.log("Status: CONNECTED!");
+  } catch (error) {
+    console.log("Statius: FAILED!");
+  }
 });
